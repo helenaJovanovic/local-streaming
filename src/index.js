543d477ef8c4b6577ca964a5c93e890bc3157835
helenaJovanovic <41912173+helenaJovanovic@users.noreply.github.com>
2020-12-10 20:30:02 +0100

    feat: made Navbar and initial routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import './index.css'
+import './moduleLink.css'
+const Start = () => <h1>Start</h1>
+const chooseVideo = () => <h1>Series</h1>
+const About = () => <h1>About</h1>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class NavBar extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {LinkStyle: ""}
+    this.state.LinkStyle = {display: 'block',
+      color: 'white',
+      textAlign: 'center',
+      padding: '14px 16px',
+      textDecoration: 'none'
+    };
+  }
+
+  onHoverr = () =>{
+    this.setState({LinkStyle: {display: 'block',
+    color: 'gray',
+    textAlign: 'center',
+    padding: '14px 16px',
+    textDecoration: 'none'
+    }}
+    );
+  };
+
+  render() {
+    return (
+      <ul class="navlist">
+        <li class="lstel">
+          <Link className="Link" to="/">Start</Link>
+          </li>
+        <li class="lstel"><Link className="Link" to="/series">Series</Link></li>
+        <li class="lstel"><Link className="Link" to="/about">About</Link></li>
+      </ul> 
+      )
+  }
+}
+
+class App extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path='/' component={Start} />
+          <Route path='/series' component={chooseVideo} />
+          <Route path="/about" component={About}/>
+        </div>
+      </Router>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
